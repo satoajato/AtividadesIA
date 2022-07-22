d32990c5f1d12ad02cd84587e21123eb355be194
Lucas Sato <62661137+satoajato@users.noreply.github.com>
2022-07-22 18:45:00 -0300

    Delete NQueen directory

--- NQueen/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        int number = 7; //args
-        NQueen jogo = new NQueen(number);
-        jogo.getTabuleiro().printarTabuleiro();
-
-        jogo.solucao();
-    }
-}
